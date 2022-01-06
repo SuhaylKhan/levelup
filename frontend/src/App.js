b54be4c77f4ccaf05f963baa053d3805932c942a
suhaylkhan <suhaylmkhan@gmail.com>
2022-01-06 17:52:11 -0600

    added styles to login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
 
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path="/">
+        <h1>HELLO WORLD</h1>
+      </Route>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,40 +1,62 @@
-import { useDispatch } from "react-redux";
-import { useState } from "react";
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-import { login } from "../../store/session";
+import './LoginForm.css';
 
-const LoginFormPage = () => {
+function LoginFormPage() {
   const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const [credential, setCredential] = useState("");
-  const [password, setPassword] = useState("");
+  let errorUl
+
+  if (sessionUser) return (
+    <Redirect to="/" />
+  );
+
+  if (errors.length > 0) {
+    errorUl =
+      <ul>
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      </ul>
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
+  }
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        dispatch(login({ credential, password }));
-      }}
-    >
-      <label for="credential">Username or Email</label>
-      <input
-        type="text"
-        name="credential"
-        value={credential}
-        onChange={(e) => setCredential(e.target.value)}
-      />
-
-      <label for="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username or Email
+        <input
+          type="text"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      {errorUl}
+      <button type="submit">Log In</button>
     </form>
   );
-};
+}
 
 export default LoginFormPage;
