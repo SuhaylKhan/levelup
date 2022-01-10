8e1c310f6104c42885ea8572eeca32aa31591dc4
suhaylkhan <suhaylmkhan@gmail.com>
2022-01-10 07:47:52 -0600

    fixed grammar on splash page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -6,7 +6,7 @@ function Splash() {
     <div className="splash-body">
       <div className="text">
         <h1>Gaming is better with a buddy. Level up your gaming experience.</h1>
-        <p>FPS, MOBA CO-OP, whatever you're looking for, get out of the solo queue and Levelup.</p>
+        <p>FPS, MOBA, CO-OP, whatever you're looking for, get out of the solo queue and Levelup.</p>
       </div>
       <div className="image">
         <img src={gamingPic} alt="Buddies gaming" height="300px" />
