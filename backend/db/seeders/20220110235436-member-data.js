77bddcdd22d42c13b2244bef5215f578ce96a6d1
suhaylkhan <suhaylmkhan@gmail.com>
2022-01-10 18:02:39 -0600

    added seeds to members

---
+++ backend/db/seeders/20220110235436-member-data.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Members', [
+      {
+        userId: 1,
+        groupId: 1
+      },
+      {
+        userId: 3,
+        groupId: 1
+      },
+      {
+        userId: 3,
+        groupId: 3
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Members', {
+      userId: { [Op.in]: [1, 3] }
+    }, {});
+  }
+};
