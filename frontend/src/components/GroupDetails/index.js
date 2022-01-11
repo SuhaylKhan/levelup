b232460a792f4e947318923d0e653b825a698784
suhaylkhan <suhaylmkhan@gmail.com>
2022-01-11 16:03:30 -0600

    created basic structure for group details page

--- frontend/src/components/GroupDetails/index.js
+++ frontend/src/components/GroupDetails/index.js
@@ -1,12 +1,30 @@
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function GroupDetails() {
   const params = useParams();
   const { groupId } = params;
+  const group = useSelector((state) => state.groups[groupId])
+  const sessionUser = useSelector((state) => state.session.user)
+
+  let createEvent;
+
+  if (sessionUser.id === group.adminId) {
+    createEvent = (
+      <button>Create an Event</button>
+    )
+  }
 
   return (
     <>
-      <h1>GROUP DETAILS COMPONENT</h1>
+      <div className='group-details'>
+        <h1>{group.name}</h1>
+        <p>{group.description}</p>
+      </div>
+      <div className='group-event'>
+        <h2>Upcoming Events</h2>
+        {createEvent}
+      </div>
     </>
   )
 };
--- frontend/src/components/Groups/index.js
+++ frontend/src/components/Groups/index.js
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom';
 
 function Groups() {
   const groups = useSelector((state) => state.groups);
-  console.log(Object.entries(groups))
 
   return (
     <>
-      <h1>GROUPS COMPONENT</h1>
       {Object.entries(groups).map((ele) => {
         const group = ele[1]
         return (
-          <Link to={`/groups/${group.id}`}>
+          <Link key={group.id} to={`/groups/${group.id}`}>
             <h2>{group.name}</h2>
             <p>{group.description}</p>
           </Link>
