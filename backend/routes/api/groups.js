ae215cac0713efba5d0afa93db4011b13af6ef76
suhaylkhan <suhaylmkhan@gmail.com>
2022-01-11 15:12:47 -0600

    set up groups store

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -5,6 +5,15 @@ const { Group } = require('../../db/models');
 const router = express.Router();
 
 router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const groups = await Group.findAll();
+
+    return res.json({ groups });
+  })
+);
+
+router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const groupId = req.params.id;
---
+++ frontend/src/store/groups.js
@@ -0,0 +1,36 @@
+import { csrfFetch } from './csrf';
+
+const SET_GROUPS = 'groups/setGroups';
+
+const setGroups = (groups) => {
+  return {
+    type: SET_GROUPS,
+    payload: groups
+  };
+};
+
+export const getGroups = () => async (dispatch) => {
+  const response = await csrfFetch('/api/groups');
+
+  const data = await response.json();
+  dispatch(setGroups(data.groups));
+  return response;
+}
+
+const groupsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case SET_GROUPS: {
+      const newState = action.payload.reduce((a, b) => {
+        return {
+          ...a,
+          [b.id]: b
+        }
+      }, {})
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default groupsReducer;
