d34160a0752dea60f0244bdf487cb71dd94513f7
suhaylkhan <suhaylmkhan@gmail.com>
2022-01-12 15:09:47 -0600

    completed basic structure of user profile

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -19,24 +19,40 @@ function UserProfile({ sessionUser }) {
     <>
       {isLoaded && (
         <>
-          <h1>Your Groups</h1>
-          {Object.entries(userGroups).map(ele => {
-            const group = ele[1];
-            return (
-              <Link key={group.id} to={`/groups/${group.id}`}>
-                <h2>{group.name}</h2>
-                <p>{group.description}</p>
-              </Link>
-            )
-          })}
-          <div>
-            Eager for another gaming sesh?
-            <button
-              onClick={() => history.push('/groups')}
-            >
-              Browse All Groups
-            </button>
-          </div>
+          {Object.keys(userGroups) > 0 ?
+            <>
+              <h1>Your Groups</h1>
+              {Object.entries(userGroups).map(ele => {
+                const group = ele[1];
+                return (
+                  <Link key={group.id} to={`/groups/${group.id}`}>
+                    <h2>{group.name}</h2>
+                    <p>{group.description}</p>
+                  </Link>
+                )
+              })}
+              <div>
+                Eager for another gaming sesh?
+                <button
+                  onClick={() => history.push('/groups')}
+                >
+                  Browse All Groups
+                </button>
+              </div>
+            </>
+            :
+            <>
+              <h1>Welcome to Levelup!</h1>
+              <div>
+                Find your squad
+                <button
+                  onClick={() => history.push('/groups')}
+                >
+                  Join a Group
+                </button>
+              </div>
+            </>
+          }
         </>
       )}
     </>
